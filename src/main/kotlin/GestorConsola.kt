20306b6fed906ad4a895f880dd1f71a43d05cde8
rmungar <mgraul@iesrafaelalberti.es>
2024-03-14 18:41:29 +0100

    Primera parte corregida

--- src/main/kotlin/GestorConsola.kt
+++ src/main/kotlin/GestorConsola.kt
@@ -2,17 +2,17 @@ package org.pebiblioteca
 
 object GestorConsola {
 
-    private val pedirTitulo = "Ingrese el título del libro: "
-    private val pedirAutor = "Ingrese el autor del libro: "
-    private val pedirPublicacion = "Ingrese la fecha de publicacion con el siguiente formato dd/mm/yyyy: "
-    private val pedirId = "Ingrese el ID del libro: "
+    private const val PedirTitulo = "Ingrese el título del libro: "
+    private const val PedirAutor = "Ingrese el autor del libro: "
+    private const val PedirPublicacion = "Ingrese la fecha de publicacion con el siguiente formato dd/mm/yyyy: "
+    private const val PedirId = "Ingrese el ID del libro: "
 
 //      PARA MOSTRAR POR PANTALLA   ------------------------------------------------------------------------------------
     fun mostrarPorConsola(texto:String){
         print(texto)
     }
 
-    fun mostrarEntradaIncorrecta(){
+    private fun mostrarEntradaIncorrecta(){
         println("Ingrese un valor adecuado.")
     }
 
@@ -26,7 +26,7 @@ object GestorConsola {
     fun comprobarTituloDelLibro(): String{
         var titulo = ""
         while (titulo.isBlank() && titulo.isEmpty()) {
-            mostrarPorConsola(pedirTitulo)
+            mostrarPorConsola(PedirTitulo)
             titulo = readln().capitalizar()
         }
         return titulo
@@ -34,7 +34,7 @@ object GestorConsola {
     fun comprobarAutorDelLibro(): String{
         var autor = ""
         while (autor.isBlank() && autor.isEmpty()) {
-            mostrarPorConsola(pedirAutor)
+            mostrarPorConsola(PedirAutor)
             autor = readln().capitalizar()
         }
         return autor
@@ -43,17 +43,17 @@ object GestorConsola {
     fun comprobarPublicacionDelLibro():String{
         var fecha:List<String> = listOf("1","1","1")
         while(fecha[0].toInt() !in 1..31 || fecha[1].toInt() !in 1..12 || fecha[2].toInt() !in 2001..2024){
-            mostrarPorConsola(pedirPublicacion)
+            mostrarPorConsola(PedirPublicacion)
             fecha = readln().split("/")
         }
         return fecha.formatear()
     }
 
-    fun comprobarIdDelLibro():String{
-        var id = ""
-        while (id !in UtilidadesBiblioteca.listaIDs){
-            mostrarPorConsola(pedirId)
-            id = readln()
+    fun comprobarIdDelLibro():Int{
+        var id = 0
+        while (id !in 100..999){
+            mostrarPorConsola(PedirId)
+            id = readln().toInt()
         }
         return id
     }
